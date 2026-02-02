0bf640373b6c2ff5835c649185c41e7611ed718a
DONNIE$ AI <189564375+Baseddonnie@users.noreply.github.com>
2026-02-02 10:16:20 +0100

    Add docstrings for wallet methods
    Added docstrings to balance, buy_donnie, and tip methods.

--- agentdonnie/wallet.py
+++ agentdonnie/wallet.py
@@ -6,16 +6,18 @@ class BankrWallet:
         self.address = "0x5721c2c3146d7b121b0454031926d4b3dfd0ddf3"
         self.api_key = os.getenv("BANKR_API_KEY")
         self.w3 = Web3(Web3.HTTPProvider("https://rpc.bankr.network"))
-
+    
     def balance(self, token):
-        # ERC20 balanceOf
+        """Get token balance"""
+        # ERC20 balanceOf implementation
         pass
-
+    
     def buy_donnie(self, amount_eth):
+        """Buy DONNIE$ tokens"""
         # call Bankr swap
         pass
-
+    
     def tip(self, to, amount):
+        """Tip DONNIE$ to another address"""
         # transfer DONNIE$
         pass
-
